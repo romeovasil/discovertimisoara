45a4ce7970a54363ccde60b26ea9261be5997578
vasilromeo <romeo.vasil@icloud.com>
2018-10-22 14:36:47 +0300

    aa

--- app/src/main/java/com/example/romeo/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/romeo/myfirstapp/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.romeo.myfirstapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun onclick(view:View)
+    {
+        var intent= Intent(this,pagina2::class.java)
+        startActivity(intent)
+    }
+    public fun onclick(view:View)
+    {
+        var intent=Intent(this,pagina3::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/romeo/myfirstapp/pagina3.kt
@@ -0,0 +1,12 @@
+package com.example.romeo.myfirstapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class pagina3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagina3)
+    }
+}
